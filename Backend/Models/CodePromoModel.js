d7689432fce9d463d76c8107c08af0608d8addc4
abdessamadbettal <abdessamadbettal@gmail.com>
2022-12-02 16:44:06 +0100

    add les models sckema produit codepromo et categorie

---
+++ Backend/Models/CodePromoModel.js
@@ -0,0 +1,44 @@
+
+const Sequelize = require('sequelize');
+const db = require('../Config/ConfigDb');
+
+const CodePromo = db.define('codepromo', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    code: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+        model: 'users', // 'Actors' would also work
+        key: 'id'
+        }
+    },
+    perproduit:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    peruser:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    produit_id: {
+        type: Sequelize.INTEGER,
+        references: {
+        model: 'produit', // 'Actors' would also work
+        key: 'id'
+        }
+    },
+    quantite:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+
+});
+
+module.exports = CodePromo;
