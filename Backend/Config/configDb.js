d00ca92b251720cb3fbf8598ebf0a61f33e95a4a
sabirkhaloufi <sabirkhaloufi@gmail.com>
2022-12-03 14:24:46 +0100

    configuration de connexion base donner postgres

---
+++ Backend/Config/configDb.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        host:"localhost",
+        dialect: 'postgres',
+    },
+  );
+  
+   try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.log(error);
+  }
+
+  module.exports = sequelize
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const dbConnection = require("./Config/configDb");
 
 port = process.env.PORT || 5000;
 
