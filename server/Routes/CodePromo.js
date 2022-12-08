9b2edfa06c024453cce890ee0d518c3013bcbb9a
amina <aminasalik012@gmail.com>
2022-12-08 16:24:54 +0100

    add route pour UpdateCodePromo

--- server/Routes/CodePromo.js
+++ server/Routes/CodePromo.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {CreatPromoCode,UpdatePromoCode} = require('../Controllers/CodePromoController');
+const {CreatPromoCode} = require('../Controllers/CodePromoController');
 
 
  
 router.post('/creatPromoCode',CreatPromoCode)
-router.post('/updatePromoCode',UpdatePromoCode)
+router.post('/UpdatePromoCode',UpdatePromoCode)
 
 
 // router.post('api/auth/login',Login)
