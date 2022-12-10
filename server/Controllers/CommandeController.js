d5e5d0dd19d0dde09b4e898af9b5a160041fe47c
amina <aminasalik012@gmail.com>
2022-12-10 23:16:28 +0100

    creat function get All commands

--- server/Controllers/CommandeController.js
+++ server/Controllers/CommandeController.js
@@ -1,44 +1,39 @@
-
 const db = require('../Models/CommandeModel');
 const Commande = require('../Models/Command_Produits');
-
 const asyncHandler = require('express-async-handler');
 
 
 
-// method : post
-// url : /api/codePromo/creatPromoCode
+// method : get
+// url : /api/commande/creatCommande
 // access : private
 
-const CreatCommande = asyncHandler(async (req, res) => {
-    
-      
-       
-        // console.log("hello api")
-       
-        const { quantite, priceTotal, code,status } = req.body;
-        if (!quantite || !priceTotal || !code || !status ) {
-            res.status(400).send('Please fill all fields.')
-        }
-    
-    try{
+    const CreatCommande = asyncHandler(async (req, res) => {
+            // console.log("hello api")
+            const { quantite, priceTotal, code,status } = req.body;
+            if (!quantite || !priceTotal || !code || !status ) {
+                res.status(400).send('Please fill all fields.')
+            }
         
-        const data = {
-            quantite: quantite,
-            priceTotal: priceTotal,
-            code: code,
-            status:status
-    
-        }
-        await db.create(data)
-        res.status(200).send({message:"creat commandes success"})
-     }catch (error) {
-            res.status(400).send('not creat commandes')
-    }
-      
-      
-    })
+        try{
+            
+            const data = {
+                quantite: quantite,
+                priceTotal: priceTotal,
+                code: code,
+                status:status
+        
+            }
+            await db.create(data)
+            res.status(200).send({message:"creat commandes success"})
+        }catch (error) {
+                res.status(400).send('not creat commandes')
+        }  
+        })
 
+// method : put
+// url : /api/commande/updateCommandes
+// access : private
     
     const UpdateCommandes = asyncHandler(async (req, res) => {
         const {old_code, NewQuantite, NewPriceTotal, NewCode, NewStatus } = req.body;
@@ -46,56 +41,53 @@ const CreatCommande = asyncHandler(async (req, res) => {
            res.status(400).send('Please fill all fields.')
        }
      
-      try{
-       await db.update(
+        try{
+            await db.update(
+        
+                {
+                    quantite: NewQuantite,
+                    priceTotal: NewPriceTotal,
+                    code: NewCode,
+                    status: NewStatus,
+                },
+                {
+                    where: { code:old_code},
+                })
+                res.status(400).send('Ubdate commands success')
    
-           {
-            quantite: NewQuantite,
-            priceTotal: NewPriceTotal,
-            code: NewCode,
-            status: NewStatus,
-           },
-           {
-               where: { code:old_code},
-           })
-           res.status(400).send('Ubdate commands success')
-   
-       }catch (error) {
+        }catch (error) {
            res.status(400).send('not update commandes')
-   }
-      
-   
-   })
+        }
+    })
+
+// method : delete
+// url : /api/commande/deleteCommande/:id
+// access : private
 
    const DeleteCommande = asyncHandler( async(req,res)=>{
-    const id =  req.params.id;
-    try {
-        const deleteCommandes = await db.destroy({where:{id}})
-        res.status(200).send({message:"delete code_Promo success"})
-    } catch (error) {
-        res.status(400)
-        throw new Error(error)
-    }
-})
+        const id =  req.params.id;
+        try {
+            const deleteCommandes = await db.destroy({where:{id}})
+            res.status(200).send({message:"delete code_Promo success"})
+        } catch (error) {
+            res.status(400)
+            throw new Error(error)
+        }
+   })
 
+// method : get
+// url : /api/commande/getAllCommands
+// access : private
    const GetAllCommands = asyncHandler( async(req,res)=>{
-    try {
-        const allCommands = await Commande.findAll()
-        res.status(200).send(allCommands)
-    } catch (error) {
-        res.status(400)
-        throw new Error(error)
-    }
-})
-
-
-
-    
-
-
-
-
-
+        try {
+            const allCommands = await Commande.findAll()
+            res.status(200).send(allCommands)
+        } catch (error) {
+            res.status(400)
+            throw new Error(error)
+        }
+   })
+   
 module.exports = {
     CreatCommande,
     DeleteCommande,
