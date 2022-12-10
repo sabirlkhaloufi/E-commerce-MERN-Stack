91e96b0690b8fe225ac11de98f895841185c72da
amina <aminasalik012@gmail.com>
2022-12-10 22:16:57 +0100

    creation functio add commande

--- server/Controllers/CommandeController.js
+++ server/Controllers/CommandeController.js
@@ -16,23 +16,24 @@ const CreatCommande = asyncHandler(async (req, res) => {
        
         // console.log("hello api")
        
-        const { code_promo, pourcentage_promo, date_expiration } = req.body;
-        if (!code_promo || !pourcentage_promo || !date_expiration) {
+        const { quantite, priceTotal, code,status } = req.body;
+        if (!quantite || !priceTotal || !code || !status ) {
             res.status(400).send('Please fill all fields.')
         }
     
     try{
         
         const data = {
-            code_promo: hashCode,
-            pourcentage_promo: pourcentage_promo,
-            date_expiration: date_expiration,
+            quantite: quantite,
+            priceTotal: priceTotal,
+            code: code,
+            status:status
     
         }
         await db.create(data)
-        res.status(200).send({message:"creat code Promo success"})
+        res.status(200).send({message:"creat commandes success"})
      }catch (error) {
-            res.status(400).send('not creat Code promo')
+            res.status(400).send('not creat commandes')
     }
       
       
