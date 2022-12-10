a6e2c80e2d584f668be3b749d8bf1e4482755eed
amina <aminasalik012@gmail.com>
2022-12-10 22:22:33 +0100

    creat functio Delet Commands

--- server/Controllers/CommandeController.js
+++ server/Controllers/CommandeController.js
@@ -38,12 +38,23 @@ const CreatCommande = asyncHandler(async (req, res) => {
       
       
     })
+    const DeleteCommande = asyncHandler( async(req,res)=>{
+        const id =  req.params.id;
+        try {
+            const deleteCommandes = await db.destroy({where:{id}})
+            res.status(200).send({message:"delete code_Promo success"})
+        } catch (error) {
+            res.status(400)
+            throw new Error(error)
+        }
+    })
 
 
 
 
 
 module.exports = {
-    CreatCommande
+    CreatCommande,
+    DeleteCommande
     
 }
